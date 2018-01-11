6774d343f0c59c57a69da1a5a2bf3db08d20fc47
Peter Ajuzie <p.ajuzie@gmail.com>
2018-01-11 15:36:51 -0600

    Completed the exceed expectations requirement
    Added and changed some codes to perfect the project and meet the exceeds expectation criteria.

--- js/quotes.js
+++ js/quotes.js
@@ -6,66 +6,77 @@ var quotes = [
     quote: 'The only way to learn it is to do it.',
     source: 'Archimedes',
     citation: 'The Sword in the Stone',
-    year: 1963
+    year: 1963,
+    tag: 'wisdom'
   },
   {
     quote: 'Nothing will work unless you do.',
     source: 'Maya Angelou',
     citation: '',
-    year: undefined
+    year: undefined,
+    tag: 'motivation'
   },
   {
     quote: 'Nothing can dim the light which shines from within',
     source: 'Maya Angelou',
     citation: '',
-    year: undefined
+    year: undefined,
+    tag: 'motivation'
   },
   {
     quote: 'Whether you think you can, or you think you can\'t, you\'re right. See you tomorrow.',
     source: 'Stewie',
     citation: 'Family Guy',
-    year: undefined
+    year: undefined,
+    tag: 'humor'
   },
   {
     quote: 'Don\'t think iPad, think IMAX.',
     source: 'Mouson Berrouet',
     citation: '',
-    year: 2014
+    year: 2014,
+    tag: 'motivation'
   },
   {
     quote: 'I\'ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.',
     source: 'Maya Angelou',
     citation: '',
-    year: undefined
+    year: undefined,
+    tag: 'love'
   },
   {
     quote: 'Live as if you were to die tomorrow. Learn as if you were to live forever.',
     source: 'Mahatma Gandhi',
     citation: '',
-    year: undefined
+    year: undefined,
+    tag: 'wisom'
   },
   {
     quote: 'Be the change that you wish to see in the world.',
     source: 'Mahatma Gandhi',
     citation: '',
-    year: undefined
+    year: undefined,
+    tag: 'politics'
   },
   {
     quote: 'We accept the love we think we deserve.',
     source: 'Stephen Chbosky',
     citation: 'The Perks of Being a Wallflower',
-    year: 1999
+    year: 1999,
+    tag: 'love'
   },
   {
     quote: 'It is better to be hated for what you are than to be loved for what you are not.',
     source: 'André Gide',
     citation: 'Autumn Leaves',
-    year:1950
+    year:1950,
+    tag: 'love'
   },
   {
     quote: 'Simplicity is the ultimate sophistication today.',
     source: 'William Gaddis',
     citation: 'The Recognitions',
-    year:1955
+    year:1955,
+    tag: 'business'
   }
 ];
--- js/safety_net.js
+++ js/safety_net.js
@@ -1,9 +1,2 @@
 // I save working code here before experimenting LOL
 // Don't judge me, code game isn't that strong, yet. ):
-
-//REFRESH PAGE AFTER A SPECIFIED TIME in milliseconds with reset timeout button
-//var timeout = setTimeout("location.reload(true);",5000);
-  // function resetTimeout() {
-  //   clearTimeout(timeout);
-  //   timeout = setTimeout("location.reload(true);",15000);
-  //}
